1e54b36aac1f23c0ad11d33ec4a0fb540a4d4018
Xiping Hu <hxp@hxp.plus>
2024-06-04 13:28:54 +0000

    将 application.properties 不打包到 jar

--- src/main/java/com/onlyoffice/integration/ExampleData.java
+++ src/main/java/com/onlyoffice/integration/ExampleData.java
@@ -24,7 +24,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
