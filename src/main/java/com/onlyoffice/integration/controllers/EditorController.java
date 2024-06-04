1bb807e477fab6d8a2c7aeeaa2b339429483769b
Xiping Hu <hxp@hxp.plus>
2024-06-04 14:10:54 +0000

    修复了用户没有登录过的Cookie时，直接打开/editor页面报400的BUG。

--- src/main/java/com/onlyoffice/integration/controllers/EditorController.java
+++ src/main/java/com/onlyoffice/integration/controllers/EditorController.java
@@ -91,8 +91,8 @@ public class EditorController {
                         @RequestParam(value = "actionLink", required = false) final String actionLink,
                         @RequestParam(value = "directUrl", required = false,
                                 defaultValue = "false") final Boolean directUrl,
-                        @CookieValue(value = "uid") final String uid,
                         final Model model) throws JsonProcessingException {
+        final String uid = "1";
         final String lang = "zh";
         Action action = Action.edit;
         Type type = Type.desktop;
