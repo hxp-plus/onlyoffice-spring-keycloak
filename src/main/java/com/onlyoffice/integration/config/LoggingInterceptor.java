6e843685aeaf18d7502046eeed79379b3d77302a
Xiping Hu <hxp@hxp.plus>
2024-06-20 22:59:24 +0800

    增加了接口调用日志。

---
+++ src/main/java/com/onlyoffice/integration/config/LoggingInterceptor.java
@@ -0,0 +1,20 @@
+package com.onlyoffice.integration.config;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggingInterceptor extends HandlerInterceptorAdapter {
+    private final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String method = request.getMethod();
+        String path = request.getRequestURI();
+        String queryString = request.getQueryString();
+        String remoteAddr = request.getRemoteAddr();
+        logger.info("Received request from {} : {} {}{}", remoteAddr,method, path, (queryString != null ? "?" + queryString : ""));
+        return true;
+    }
+}
